ea349d9c3967c0f4541d8e1ccb9e99906f43bacd
Saurav02022 <sk729584@gmail.com>
2026-08-23 15:10:34 +0530

    chapter 1: hello world and comments

---
+++ Chapter 1/main.py
@@ -0,0 +1,12 @@
+print('Hello world !')
+print('Hi, I am saurav kumar with 3+ YOE as a Software Engineer.')
+
+# This is how we comment any code in python. in mac we need to use command + / and line will comment in. comment out we use same command.
+
+# print('I am your boss.')
+
+# Multi line comment
+
+'''
+I am multi line comment.
+'''
