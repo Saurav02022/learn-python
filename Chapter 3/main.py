66b93b82401f12468435d2d7df2df1ac43c10c4d
Saurav02022 <sk729584@gmail.com>
2026-08-28 14:58:23 +0530

    chapter 3: strings, slicing and escape sequences

---
+++ Chapter 3/main.py
@@ -0,0 +1,27 @@
+name = 'Saurav'
+
+# Length of string
+print(len(name))
+
+# String slicing [start:end]
+name_short = name[2:4]
+print(name_short)
+
+# Minus indexing
+nameshort = name[-4:-1]
+print(nameshort)
+
+print(name[:4])
+print(name[0:])
+
+# Slicing with skip value
+num = "123456789"
+
+print(num[1:5:2])
+
+# Escape sequences
+
+print("Hello\nSaurav")
+print("Hello\tSaurav")
+print("He said \"Hello\"")
+print("It's Python")
